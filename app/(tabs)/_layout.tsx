e8777388506facd052c3260ad0f8a4333e5c2984
snavemail <98936310+snavemail@users.noreply.github.com>
2024-12-11 02:09:00 -0500

    Added pages / tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,14 +9,41 @@ export default function TabLayout() {
         tabBarActiveTintColor: 'black',
       }}>
       <Tabs.Screen
+        name="feed"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon2 activeName="home" inactiveName="home" focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon2 activeName="search" inactiveName="search" focused={focused} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
         name="index"
         options={{
+          headerShown: false,
           title: 'Maps',
           tabBarIcon: ({ focused }) => <TabBarIcon2 activeName="map" focused={focused} />,
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="saved"
+        options={{
+          title: 'Saved',
+          tabBarIcon: ({ focused }) => <TabBarIcon2 activeName="bookmark" focused={focused} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
         options={{
           title: 'Profile',
           tabBarIcon: ({ focused }) => <TabBarIcon2 activeName="user" focused={focused} />,
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function Profile() {
+  return (
+    <View>
+      <Text className="text-xl font-bold">Profile</Text>
+    </View>
+  );
+}
