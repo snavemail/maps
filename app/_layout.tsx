f1eef27a3b1c9d09957bd6edbb7846d9f3520f09
snavemail <98936310+snavemail@users.noreply.github.com>
2024-12-11 19:24:30 -0500

    RNGH stuff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'react-native';
 import '../global.css';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { Stack } from 'expo-router';
+import React from 'react';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -10,11 +12,15 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar barStyle={'dark-content'} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="journey"
+          options={{ animation: 'slide_from_bottom', headerShown: false }}
+        />
       </Stack>
-    </>
+    </GestureHandlerRootView>
   );
 }
