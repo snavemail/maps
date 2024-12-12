d1eb0f79944829b9da6b06cf12b517df4433003a
snavemail <98936310+snavemail@users.noreply.github.com>
2024-12-12 03:59:26 -0500

    Create a page to render a specific map with bottomsheet

--- app/journey/[slug].tsx
+++ app/journey/[slug].tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { View, Pressable } from 'react-native';
+import React, { useRef } from 'react';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
+import JourneyMap from '~/components/JourneyMap';
+import { journeys } from '~/data/journeys';
+import JourneyMapBottomSheet from '~/components/JourneyMapBottomSheet';
+import { Camera } from '@rnmapbox/maps';
 
 export default function Journey() {
+  const router = useRouter();
   const { slug } = useLocalSearchParams();
+  const journey = journeys.find((journey: any) => journey.journeyID === Number(slug));
+  const cameraRef = useRef<Camera>(null);
   return (
-    <View>
-      <Text>Journey: {slug}</Text>
-    </View>
+    <>
+      <View className={`z-0 flex min-h-full flex-1`}>
+        <JourneyMap journey={journey} cameraRef={cameraRef} />
+      </View>
+      <JourneyMapBottomSheet journey={journey} cameraRef={cameraRef} />
+      <Pressable onPress={() => router.dismiss()} className="absolute left-8 top-20 z-50">
+        <FontAwesome name="close" size={30} color="white" />
+      </Pressable>
+    </>
   );
 }
