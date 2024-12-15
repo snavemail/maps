955ed25d8a59640c9a8083c3930a2f478354667b
snavemail <98936310+snavemail@users.noreply.github.com>
2024-12-15 02:22:06 -0500

    Type check

--- components/MapMarker.tsx
+++ components/MapMarker.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { FontAwesome } from '@expo/vector-icons';
 import { PointAnnotation } from '@rnmapbox/maps';
 
-export default function MapMarker({ location }: any) {
+type MapMarkerLocation = Pick<DraftLocation | LocationInfo, 'id' | 'coordinates'>;
+
+interface MapMarkerProps {
+  location: MapMarkerLocation;
+}
+
+export default function MapMarker({ location }: MapMarkerProps) {
   return (
     <PointAnnotation
       key={location.id}
