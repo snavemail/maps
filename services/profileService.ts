5073512f42cbb4d00363f6c129da1fc7327a718b
snavemail <liam.jk.evans@gmail.com>
2024-12-20 04:45:46 -0500

    Implement avatar upload and management functionality in profileService
    - Added methods to upload, remove, and save avatar URLs for user profiles.
    - Implemented logic to ensure old avatars are removed before uploading new ones to prevent caching issues.
    - Enhanced error handling for avatar URL updates and deletions.
    - Utilized base64 decoding and Expo FileSystem for handling avatar files.

--- services/profileService.ts
+++ services/profileService.ts
@@ -1,5 +1,6 @@
 import { supabase } from '~/lib/supabase';
 import { useAuthStore } from '~/stores/useAuth';
+import { decode } from 'base64-arraybuffer';
 
 export const profileService = {
   getById: async (id: string) => {
@@ -29,5 +30,48 @@ export const profileService = {
       console.error('Error updating profile:', error);
       throw error;
     }
+  },
+
+  removeAvatar: async (filename: string) => {
+    await supabase.storage.from('avatars').remove([filename]);
+  },
+
+  uploadAvatar: async (base64: string, contentType: string, userID: string) => {
+    // get current avatar list (should be only one) and remove it
+    const { data: currentData } = await supabase.storage.from('avatars').list(userID);
+    await profileService.removeAvatar(`${userID}/${currentData?.[0]?.name}`);
+    // upload new avatar with unique filename
+    // Need unique filename because I believe supabase caches the file by the filename
+    // If I don't remove the old file, the new file will be cached and not updated
+    const uniqueFilename = `${userID}/${Date.now()}.png`;
+
+    const { error } = await supabase.storage
+      .from('avatars')
+      .upload(uniqueFilename, decode(base64), { contentType });
+    if (error) throw error;
+    const {
+      data: { publicUrl },
+    } = supabase.storage.from('avatars').getPublicUrl(uniqueFilename);
+    return publicUrl;
+  },
+
+  saveAvatarUrl: async (userID: string, avatarURL: string) => {
+    const { data, error } = await supabase
+      .from('profiles')
+      .update({ avatar_url: avatarURL })
+      .eq('id', userID);
+
+    if (error) throw error;
+    return data;
+  },
+
+  deleteAvatarURL: async (userID: string) => {
+    const { data, error } = await supabase
+      .from('profiles')
+      .update({ avatar_url: null })
+      .eq('id', userID);
+
+    if (error) throw new Error(`Error deleting avatar URL: ${error.message}`);
+    return data;
   },
 };
