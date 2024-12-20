b86157112128fba1b751f6de235c665f7c963de6
snavemail <liam.jk.evans@gmail.com>
2024-12-20 04:43:32 -0500

    Refactor Publish component to enhance UI and streamline functionality
    - Removed unused imports and commented-out code for improved clarity.
    - Replaced KeyboardAvoidingView with SafeAreaView for better layout management.
    - Updated state management to handle title and description separately.
    - Integrated MainMapNonInteractive and LocationCard components for a more organized structure.
    - Enhanced form inputs for journey title and description with improved styling and functionality.
    - Added conditional rendering for the Publish and Discard buttons based on form state.

--- app/(tabs)/map/publish.tsx
+++ app/(tabs)/map/publish.tsx
@@ -2,25 +2,19 @@ import {
   View,
   Text,
   KeyboardAvoidingView,
-  ActivityIndicator,
   Platform,
   Pressable,
   ScrollView,
-  Image,
-  Switch,
   TextInput,
   Alert,
+  SafeAreaView,
 } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'expo-router';
 import { FontAwesome } from '@expo/vector-icons';
-import { ImageManipulator } from 'expo-image-manipulator';
-import { getTitle } from '~/lib/utils';
 import { useJourneyStore } from '~/stores/useJourney';
-import { useUserLocationStore } from '~/stores/useUserLocation';
-import * as Location from 'expo-location';
-import * as ImagePicker from 'expo-image-picker';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import MainMapNonInteractive from '~/components/Maps/MainMapNonInteractive';
+import LocationCard from '~/components/PublishLocationCard';
 
 interface LocationData {
   isUpdate: boolean;
@@ -37,7 +31,8 @@ export default function Publish() {
   const endJourney = useJourneyStore((state) => state.endJourney);
   const draftJourney = useJourneyStore((state) => state.draftJourney);
 
-  const [form, setForm] = useState<any>(null);
+  const [title, setTitle] = useState(draftJourney?.title);
+  const [description, setDescription] = useState(draftJourney?.description);
   const router = useRouter();
 
   const onClose = () => {
@@ -65,149 +60,73 @@ export default function Publish() {
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      className="flex-1 justify-end">
-      <ScrollView bounces stickyHeaderIndices={[0]} className="bg-white pb-8">
-        <View className="flex-row items-center justify-between border-b border-gray-200 bg-white p-4">
-          <Text className="text-xl font-semibold">Save Journey</Text>
-          <Pressable onPress={onClose} hitSlop={8} className="active:scale-95">
-            <FontAwesome name="times" size={24} color="black" />
-          </Pressable>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+    <SafeAreaView className="flex-1">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1 justify-end">
+        <ScrollView bounces stickyHeaderIndices={[0]} className="bg-white pb-8">
+          <View className="flex-row items-center justify-between border-b border-gray-200 bg-white p-4">
+            <Text className="text-xl font-semibold">Save Journey</Text>
+            <Pressable onPress={onClose} hitSlop={8} className="active:scale-95">
+              <FontAwesome name="times" size={24} color="black" />
+            </Pressable>
+          </View>
+          <View className="flex h-64 w-full bg-black">
+            <MainMapNonInteractive />
+          </View>
+
+          <View className="p-4">
+            <View className="gap-y-4">
+              <View>
+                <Text className="mb-1 text-sm text-gray-600">Title</Text>
+                <TextInput
+                  value={draftJourney?.title}
+                  onChangeText={(text) => setTitle(text)}
+                  className="rounded-lg border border-gray-200 p-3"
+                  placeholder="Enter journey title"
+                />
+              </View>
+
+              <View>
+                <Text className="mb-1 text-sm text-gray-600">Description</Text>
+                <TextInput
+                  value={draftJourney?.description}
+                  onChangeText={(text) => setDescription(text)}
+                  className="rounded-lg border border-gray-200 p-3"
+                  placeholder="Enter journey description"
+                  multiline
+                  numberOfLines={4}
+                />
+              </View>
+
+              <View>
+                <Text className="mb-1 text-sm text-gray-600">Start Date</Text>
+              </View>
+
+              <View>
+                <Text className="mb-3 text-sm text-gray-600">
+                  Locations ({draftJourney?.locations.length})
+                </Text>
+                {draftJourney?.locations.map((location) => (
+                  <LocationCard key={location.id} location={location} />
+                ))}
+              </View>
+            </View>
+
+            <View className="flex flex-col gap-4 py-4">
+              <Pressable
+                onPress={handleSubmit}
+                className="flex-1 items-center justify-center rounded-lg bg-black px-3 py-3 active:bg-[#1f1f1f]"
+                disabled={!draftJourney?.title || draftJourney?.locations.length === 0}>
+                <Text className="text-center font-semibold text-white">Publish Journey</Text>
+              </Pressable>
+              <Pressable onPress={onDiscard} className="flex-1 items-center justify-center">
+                <Text className="text-center font-semibold text-red-500 underline">Discard</Text>
+              </Pressable>
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
-
-// return (
-//   <KeyboardAvoidingView
-//     behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-//     className="flex-1 justify-end">
-//     <ScrollView bounces stickyHeaderIndices={[0]} className="bg-white pb-8">
-//       <View className="flex-row items-center justify-between border-b border-gray-200 bg-white p-4">
-//         <Text className="text-xl font-semibold">Save Journey</Text>
-//         <Pressable onPress={onClose} hitSlop={8} className="active:scale-95">
-//           <FontAwesome name="times" size={24} color="black" />
-//         </Pressable>
-//       </View>
-
-//       <View className="p-4">
-//         <View className="gap-y-4">
-//           <View>
-//             <Text className="mb-1 text-sm text-gray-600">Journey Title</Text>
-//             <TextInput
-//               value={journey.title}
-//               onChangeText={(text) => setJourney((prev) => ({ ...prev, title: text }))}
-//               className="rounded-lg border border-gray-200 p-3"
-//               placeholder="Enter journey title"
-//             />
-//           </View>
-
-//           <View>
-//             <Text className="mb-1 text-sm text-gray-600">Description</Text>
-//             <TextInput
-//               value={journey.description}
-//               onChangeText={(text) => setJourney((prev) => ({ ...prev, description: text }))}
-//               className="rounded-lg border border-gray-200 p-3"
-//               placeholder="Enter journey description"
-//               multiline
-//               numberOfLines={4}
-//             />
-//           </View>
-
-//           <View>
-//             <Text className="mb-1 text-sm text-gray-600">Start Date</Text>
-//             <Pressable
-//               onPress={() => setShowDatePicker(true)}
-//               className="rounded-lg border border-gray-200 p-3">
-//               <Text>{new Date(journey.startDate).toLocaleString()}</Text>
-//             </Pressable>
-
-//             <DateTimePickerModal
-//               isVisible={showDatePicker}
-//               buttonTextColorIOS="#000"
-//               mode="datetime"
-//               onConfirm={handleConfirm}
-//               onCancel={() => setShowDatePicker(false)}
-//               date={new Date(journey.startDate)}
-//             />
-//           </View>
-
-//           <View>
-//             <Text className="mb-3 text-sm text-gray-600">
-//               Locations ({journey.locations.length})
-//             </Text>
-//             {journey.locations.map((location) => (
-//               <LocationCard key={location.id} location={location} />
-//             ))}
-//           </View>
-//         </View>
-
-//         <View className="flex flex-col gap-4 py-4">
-//           <Pressable
-//             onPress={handleSubmit}
-//             className="flex-1 items-center justify-center rounded-lg bg-black px-3 py-3 active:bg-[#1f1f1f]"
-//             disabled={!journey.title || journey.locations.length === 0}>
-//             <Text className="text-center font-semibold text-white">
-//               Publish Journey
-//             </Text>
-//           </Pressable>
-//           <Pressable onPress={onClose} className="flex-1 items-center justify-center">
-//             <Text className="text-center font-semibold text-black underline">
-//               Cancel
-//             </Text>
-//           </Pressable>
-//         </View>
-//       </View>
-//     </ScrollView>
-//   </KeyboardAvoidingView>
-// );
-// const LocationCard = ({ location }: { location: DraftLocation }) => {
-//   return (
-//     <View className="mb-3 rounded-lg border border-gray-200 p-3">
-//       <View className="flex-row justify-between">
-//         <Text className="font-medium">{location.title}</Text>
-//         <Text className="text-xs text-gray-500">
-//           {new Date(location.date).toLocaleDateString()}
-//         </Text>
-//       </View>
-
-//       {location.description && (
-//         <Text className="mt-1 text-sm text-gray-600" numberOfLines={2}>
-//           {location.description}
-//         </Text>
-//       )}
-
-//       {location.images.length > 0 && (
-//         <ScrollView
-//           horizontal
-//           showsHorizontalScrollIndicator={false}
-//           className="mt-2"
-//           contentContainerStyle={{ gap: 8 }}
-//         >
-//           {location.images.map((image, index) => (
-//             <Image
-//               key={index}
-//               source={{ uri: image }}
-//               className="h-16 w-16 rounded-md"
-//             />
-//           ))}
-//         </ScrollView>
-//       )}
-
-//       <View className="mt-2 flex-row items-center justify-between">
-//         <Text className="text-xs text-gray-500">
-//           {!location.hideLocation && location.address}
-//         </Text>
-//         {location.rating > 0 && (
-//           <View className="flex-row">
-//             {[...Array(location.rating)].map((_, i) => (
-//               <FontAwesome key={i} name="star" size={12} color="black" />
-//             ))}
-//           </View>
-//         )}
-//       </View>
-//     </View>
-//   );
-// };
