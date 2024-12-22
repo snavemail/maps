b02e9ceb50c93449a12a23ab2b26efd9489f8470
snavemail <liam.jk.evans@gmail.com>
2024-12-22 13:19:12 -0500

    Enhance MainMapNonInteractive component with improved map projection and camera handling
    - Added useRef and useEffect hooks for better camera reference management.
    - Updated map projection from 'globe' to 'mercator' for improved visualization.
    - Wrapped MapView in a View component to control height.
    - Adjusted camera bounds and padding for better map display.
    - Ensured MainMapMarker is rendered in non-interactive mode.
    
    These changes improve the user experience and maintainability of the map component.

--- components/Maps/MainMapNonInteractive.tsx
+++ components/Maps/MainMapNonInteractive.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useRef, useEffect } from 'react';
 import Mapbox, { Camera, MapView } from '@rnmapbox/maps';
 import { useJourneyStore } from '~/stores/useJourney';
 import LineSegment from '~/components/LineSegment';
-import { getBounds } from '~/utils/MapBox';
+import { centerOnCoordinates, getBounds } from '~/utils/MapBox';
 import { usePreferenceStore } from '~/stores/usePreferences';
 import { PADDINGCONFIG } from '~/constants/mapbox';
 import MainMapMarker from '~/components/MainMapMarker';
+import { CameraRef } from '@rnmapbox/maps/lib/typescript/src/components/Camera';
+import { View } from 'react-native';
 
 export default function MainMapNonInteractive() {
   const accessToken = process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN;
@@ -17,6 +19,7 @@ export default function MainMapNonInteractive() {
   const mapTheme = usePreferenceStore((state) => state.mapTheme);
   const draftJourney = useJourneyStore((state) => state.draftJourney);
   const [loaded, setLoaded] = useState(false);
+  const cameraRef = useRef<CameraRef>(null);
 
   const sortedLocations = useMemo(() => {
     return (
@@ -40,38 +43,40 @@ export default function MainMapNonInteractive() {
 
   Mapbox.setAccessToken(accessToken);
   return (
-    <MapView
-      projection="globe"
-      style={{ flex: 1 }}
-      styleURL={mapTheme}
-      logoEnabled={true}
-      compassEnabled={false}
-      zoomEnabled={false}
-      pitchEnabled={false}
-      rotateEnabled={false}
-      scrollEnabled={false}
-      attributionEnabled={false}
-      logoPosition={{ bottom: 0, left: 0 }}
-      attributionPosition={{ bottom: 0, left: 100 }}
-      onDidFinishLoadingMap={() => {
-        setLoaded(true);
-      }}
-      scaleBarEnabled={false}>
-      <Camera
-        zoomLevel={13}
-        bounds={{ ne: [bounds.maxLon, bounds.maxLat], sw: [bounds.minLon, bounds.minLat] }}
-        animationMode="none"
-        padding={{
-          paddingLeft: PADDINGCONFIG[3],
-          paddingRight: PADDINGCONFIG[1],
-          paddingTop: PADDINGCONFIG[0],
-          paddingBottom: PADDINGCONFIG[2],
+    <View className="h-64">
+      <MapView
+        projection="mercator"
+        style={{ flex: 1 }}
+        styleURL={mapTheme}
+        logoEnabled={true}
+        compassEnabled={false}
+        zoomEnabled={false}
+        pitchEnabled={false}
+        rotateEnabled={false}
+        scrollEnabled={false}
+        attributionEnabled={false}
+        logoPosition={{ bottom: 0, left: 0 }}
+        attributionPosition={{ bottom: 0, left: 100 }}
+        onDidFinishLoadingMap={() => {
+          setLoaded(true);
         }}
-        animationDuration={0}
-      />
-      {sortedLocations.length > 1 && <LineSegment coordinates={coordinates} />}
-
-      <MainMapMarker locations={sortedLocations} />
-    </MapView>
+        scaleBarEnabled={false}>
+        <Camera
+          zoomLevel={13}
+          bounds={{
+            ne: [bounds.maxLon, bounds.maxLat],
+            sw: [bounds.minLon, bounds.minLat],
+            paddingLeft: 25,
+            paddingRight: 25,
+            paddingTop: 25,
+            paddingBottom: 25,
+          }}
+          animationMode="none"
+          animationDuration={0}
+        />
+        {sortedLocations.length > 1 && <LineSegment coordinates={coordinates} />}
+        <MainMapMarker locations={sortedLocations} nonInteractive={true} />
+      </MapView>
+    </View>
   );
 }
