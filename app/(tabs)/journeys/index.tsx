fa38967dd2532d0cb5c74b9c402482bff68ffeb8
snavemail <liam.jk.evans@gmail.com>
2024-12-23 00:24:11 -0500

    Implement journey fetching and display in Journeys component
    - Added state management to handle journeys and loading state.
    - Integrated journeyService to fetch user-specific journeys.
    - Replaced static content with a FlatList to dynamically render JourneyPreview components.
    - Ensured the component returns null if the user profile is not available, enhancing user experience.
    
    These changes improve the functionality of the Journeys component by providing a dynamic list of journeys tailored to the authenticated user.

--- app/(tabs)/journeys/index.tsx
+++ app/(tabs)/journeys/index.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { journeyService } from '~/services/journeyService';
+import { useAuthStore } from '~/stores/useAuth';
+import JourneyPreview from '~/components/JourneyPreview';
 
-export default function Saved() {
+export default function Journeys() {
+  const [journeys, setJourneys] = useState<JourneyWithProfile[]>([]);
+  const profile = useAuthStore((state) => state.profile);
+  const [loading, setLoading] = useState(true);
+  if (!profile) {
+    return null;
+  }
+
+  useEffect(() => {
+    fetchMyJourneys();
+  }, []);
+
+  const fetchMyJourneys = async (): Promise<void> => {
+    const myJourneys = await journeyService.fetchMyJourneys();
+    setJourneys(myJourneys);
+  };
+
   return (
-    <View>
-      <Text>Saved</Text>
-    </View>
+    <FlatList
+      data={journeys}
+      renderItem={({ item }: { item: JourneyWithProfile }) => <JourneyPreview journey={item} />}
+      keyExtractor={(item: JourneyWithProfile) => item.id}
+    />
   );
 }
