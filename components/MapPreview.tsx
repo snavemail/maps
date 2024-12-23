e9677eda1b24adfe73f318e909f8dbc0ab7ee9c3
snavemail <liam.jk.evans@gmail.com>
2024-12-23 00:26:18 -0500

    Refactor MapPreview component for improved functionality and performance
    - Replaced useRef with useState and useMemo for better state management and performance optimization.
    - Enhanced location sorting by date and updated coordinate extraction logic.
    - Implemented dynamic camera positioning based on the number of locations, improving user experience.
    - Added loading state management for the map view to ensure smoother rendering.
    - Updated MapView properties for better usability and visual consistency.
    
    These changes enhance the MapPreview component's functionality and user experience by optimizing rendering and improving the handling of journey locations.

--- components/MapPreview.tsx
+++ components/MapPreview.tsx
@@ -1,54 +1,95 @@
-import React, { useRef } from 'react';
-import Mapbox, { Camera, MapView, PointAnnotation, StyleURL, SymbolLayer } from '@rnmapbox/maps';
-import LineSegment from './LineSegment';
-import MapMarker from './Markers/MapMarker';
+import React, { useState, useMemo } from 'react';
+import Mapbox, { Camera, MapView } from '@rnmapbox/maps';
+import LineSegment from '~/components/LineSegment';
+import { getBounds } from '~/utils/MapBox';
 import { usePreferenceStore } from '~/stores/usePreferences';
+import { View } from 'react-native';
+import MapMarker from './Markers/MapMarker';
 
-export default function MapPreview({ journey }: any) {
+export default function MapPreview({ journey }: { journey: JourneyWithProfile }) {
   const accessToken = process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN;
-  if (!accessToken) {
-    throw new Error('Please provide a Mapbox access token!');
-  }
-  const mapTheme = usePreferenceStore((state) => state.mapTheme);
-  const cameraRef = useRef<Camera>(null);
-  const locations = journey.locations.sort((a: any, b: any) => a.position - b.position);
-  const coordinates = locations.map((location: any) => [
-    location.coordinates.longitude,
-    location.coordinates.latitude,
-  ]);
 
-  const [minLon, minLat, maxLon, maxLat] = coordinates.reduce(
-    (bounds: number[], [lon, lat]: any) => [
-      Math.min(bounds[0], lon), // min longitude
-      Math.min(bounds[1], lat), // min latitude
-      Math.max(bounds[2], lon), // max longitude
-      Math.max(bounds[3], lat),
-    ],
-    [Infinity, Infinity, -Infinity, -Infinity]
-  );
+  if (!accessToken) {
+    throw new Error('Please provide a Mapbox access token');
+  }
+
+  const mapTheme = usePreferenceStore((state) => state.mapTheme);
+  const [loaded, setLoaded] = useState(false);
+
+  const sortedLocations = useMemo(() => {
+    return (
+      journey.locations.sort(
+        (a: LocationInfo, b: LocationInfo) =>
+          new Date(a.date).getTime() - new Date(b.date).getTime()
+      ) || []
+    );
+  }, [journey]);
+
+  const coordinates = useMemo(() => {
+    return (
+      sortedLocations?.map((location: LocationInfo) => [
+        location.coordinates.longitude,
+        location.coordinates.latitude,
+      ]) || []
+    );
+  }, [sortedLocations]);
+
+  const bounds = useMemo(() => getBounds({ coordinates }), [coordinates]);
 
   Mapbox.setAccessToken(accessToken);
   return (
-    <MapView
-      style={{ flex: 1 }}
-      styleURL={mapTheme}
-      logoEnabled={true}
-      attributionEnabled={false}
-      zoomEnabled={false}
-      scrollEnabled={false}
-      rotateEnabled={false}
-      pitchEnabled={false}
-      scaleBarEnabled={false}>
-      <Camera
-        ref={cameraRef}
-        bounds={{ ne: [maxLon, maxLat], sw: [minLon, minLat] }}
-        animationMode="easeTo"
-        padding={{ paddingLeft: 25, paddingRight: 25, paddingTop: 25, paddingBottom: 25 }}
-      />
-      <LineSegment coordinates={coordinates} />
-      {locations.map((location: any) => (
-        <MapMarker key={location.id} location={location} />
-      ))}
-    </MapView>
+    <View className="h-64">
+      <MapView
+        projection="mercator"
+        style={{ flex: 1 }}
+        styleURL={mapTheme}
+        logoEnabled={true}
+        compassEnabled={false}
+        zoomEnabled={false}
+        pitchEnabled={false}
+        rotateEnabled={false}
+        scrollEnabled={false}
+        attributionEnabled={false}
+        logoPosition={{ bottom: 0, left: 0 }}
+        attributionPosition={{ bottom: 0, left: 100 }}
+        onDidFinishLoadingMap={() => {
+          setLoaded(true);
+        }}
+        scaleBarEnabled={false}>
+        {loaded && (
+          <>
+            {sortedLocations.length === 1 ? (
+              <Camera
+                zoomLevel={13}
+                centerCoordinate={[
+                  sortedLocations[0].coordinates.longitude,
+                  sortedLocations[0].coordinates.latitude,
+                ]}
+                animationMode="none"
+                animationDuration={0}
+              />
+            ) : (
+              <Camera
+                zoomLevel={13}
+                bounds={{
+                  ne: [bounds.maxLon, bounds.maxLat],
+                  sw: [bounds.minLon, bounds.minLat],
+                  paddingLeft: 25,
+                  paddingRight: 25,
+                  paddingTop: 25,
+                  paddingBottom: 25,
+                }}
+                animationMode="none"
+                animationDuration={0}
+              />
+            )}
+            {sortedLocations.length > 1 && <LineSegment coordinates={coordinates} />}
+            {sortedLocations.map((location: any) => (
+              <MapMarker key={location.id} location={location} />
+            ))}
+          </>
+        )}
+      </MapView>
+    </View>
   );
 }
