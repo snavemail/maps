5c09d1560fe418ee4ba32bc6dd4f57cdc64fd834
snavemail <liam.jk.evans@gmail.com>
2024-12-23 22:36:36 -0500

    Refactor Profile component to improve data fetching and caching
    - Updated the Profile component to utilize profile caching, enhancing performance by reducing unnecessary API calls.
    - Replaced direct profile fetching with a cached profile retrieval method, ensuring quicker access to user data.
    - Simplified state management by integrating profile stats directly into the profile data structure.
    - Improved conditional rendering to handle cases where the profile is not available.
    
    These changes streamline the profile viewing experience and enhance the efficiency of data management.

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,40 +1,30 @@
-import { View, Text } from 'react-native';
-import { FlatList, Pressable } from 'react-native-gesture-handler';
-import ProfileHeader from '~/components/Profile/ProfileHeader';
 import { useAuthStore } from '~/stores/useAuth';
-import { useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
 import SelfProfile from '~/components/Profile/SelfProfile';
 import { profileService } from '~/services/profileService';
-
+import { useProfileCache } from '~/stores/useProfileCache';
 export default function Profile() {
-  const profile = useAuthStore((state) => state.profile);
-  if (!profile) return null;
-
-  const [stats, setStats] = useState<UserStats>({
-    totalJourneys: 0,
-    recentJourneys: 0,
-  });
-
-  const [followCounts, setFollowCounts] = useState<{ followers: number; following: number }>({
-    followers: 0,
-    following: 0,
-  });
+  const profileID = useAuthStore((state) => state.profile)?.id;
+  if (!profileID) return null;
+  const cachedProfile = useProfileCache().getProfile(profileID);
+  const [profile, setProfile] = useState<ProfileWithStats | null>(null);
 
   useEffect(() => {
-    fetchStats();
-    fetchFollowCounts();
-  }, []);
+    if (cachedProfile) {
+      setProfile(cachedProfile);
+    }
+  }, [cachedProfile]);
 
-  const fetchFollowCounts = async () => {
-    const followCounts = await profileService.fetchFollowCounts(profile.id);
-    setFollowCounts(followCounts);
+  useEffect(() => {
+    fetchProfile();
+  }, [profileID]);
+
+  const fetchProfile = async () => {
+    const profileWithStats = await profileService.fetchProfile(profileID);
+    setProfile(profileWithStats);
   };
 
-  const fetchStats = async () => {
-    const stats = await profileService.fetchStats(profile.id);
-    setStats(stats);
-  };
+  if (!profile) return null;
 
-  return <SelfProfile stats={stats} followCounts={followCounts} profile={profile} />;
+  return <SelfProfile profile={profile} />;
 }
