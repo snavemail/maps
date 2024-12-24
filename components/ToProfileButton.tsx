343aff8b32418246bc06eaf924b2e37ec3a78e12
snavemail <liam.jk.evans@gmail.com>
2024-12-23 22:37:11 -0500

    Add ToProfileButton component for profile navigation
    - Introduced a new ToProfileButton component that allows users to navigate to a specified profile based on the current route.
    - Utilizes React Native's Pressable for touch interactions and integrates with Expo Router for seamless navigation.
    - The button conditionally routes to the profile page based on the current segment, enhancing user experience in navigating profiles.
    
    This addition improves the overall navigation structure within the application, making it easier for users to access profiles directly.

---
+++ components/ToProfileButton.tsx
@@ -0,0 +1,23 @@
+import { Pressable } from 'react-native';
+import React from 'react';
+import { useRouter, useSegments } from 'expo-router';
+
+export default function ToProfileButton({
+  children,
+  profileID,
+}: {
+  children: React.ReactNode;
+  profileID: string;
+}) {
+  const segments = useSegments();
+  const router = useRouter();
+  return (
+    <Pressable
+      onPress={() => {
+        (segments[1] === 'home' || segments[1] === 'journeys' || segments[1] === 'profile') &&
+          router.push(`/(tabs)/${segments[1]}/profile/${profileID}`);
+      }}>
+      {children}
+    </Pressable>
+  );
+}
