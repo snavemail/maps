270324beef7d2d919814e05b2d4c5c62c7dd735f
snavemail <liam.jk.evans@gmail.com>
2024-12-25 09:54:12 -0500

    Make journey card go to journey page

--- components/JourneyCard.tsx
+++ components/JourneyCard.tsx
@@ -3,6 +3,7 @@ import React, { useMemo } from 'react';
 import MapPreview from './Maps/MapPreview';
 import { MapPin, UserRound, Star } from 'lucide-react-native';
 import ToProfileButton from './Buttons/ToProfileButton';
+import ToJourneyButton from './Buttons/ToJourneyButton';
 
 function JourneyCard({ journey }: { journey: JourneyWithProfile }) {
   const { dateRange, averageRating } = useMemo(() => {
@@ -36,63 +37,65 @@ function JourneyCard({ journey }: { journey: JourneyWithProfile }) {
   }, [journey]);
 
   return (
-    <View className="mb-4 overflow-hidden bg-white p-4 shadow-sm">
-      {/* Map */}
-      <View className="mb-4 h-64 rounded-lg bg-gray-200">
-        <MapPreview journey={journey} />
-      </View>
+    <ToJourneyButton journeyID={journey.id}>
+      <View className="mb-4 overflow-hidden bg-white p-4 shadow-sm">
+        {/* Map */}
+        <View className="mb-4 h-64 rounded-lg bg-gray-200">
+          <MapPreview journey={journey} />
+        </View>
 
-      {/* User Info */}
-      <ToProfileButton profileID={journey.profile.id}>
-        <View className="mb-4 flex-row items-center rounded-full border border-gray-200">
-          {journey.profile.avatar_url ? (
-            <Image
-              source={{ uri: journey.profile.avatar_url }}
-              className="h-10 w-10 rounded-full"
-            />
-          ) : (
-            <View className="h-10 w-10 items-center justify-center rounded-full bg-gray-100">
-              <UserRound size={16} color="#374151" />
+        {/* User Info */}
+        <ToProfileButton profileID={journey.profile.id}>
+          <View className="mb-4 flex-row items-center rounded-full border border-gray-200">
+            {journey.profile.avatar_url ? (
+              <Image
+                source={{ uri: journey.profile.avatar_url }}
+                className="h-10 w-10 rounded-full"
+              />
+            ) : (
+              <View className="h-10 w-10 items-center justify-center rounded-full bg-gray-100">
+                <UserRound size={16} color="#374151" />
+              </View>
+            )}
+            <View className="ml-3">
+              <View className="mb-[2px]">
+                <Text className="text-md font-semibold text-black">
+                  {journey.profile.first_name} {journey.profile.last_name}
+                </Text>
+              </View>
+              <View>
+                <Text className="text-xs text-gray-600">{dateRange}</Text>
+              </View>
+            </View>
+          </View>
+        </ToProfileButton>
+        {/* Journey Title + Description */}
+        <View className="mb-4">
+          <View className="">
+            <Text className="text-lg font-semibold text-black">{journey.title}</Text>
+          </View>
+          {journey.description && (
+            <View>
+              <Text className="text-sm text-gray-600">{journey.description}</Text>
             </View>
           )}
-          <View className="ml-3">
-            <View className="mb-[2px]">
-              <Text className="text-md font-semibold text-black">
-                {journey.profile.first_name} {journey.profile.last_name}
-              </Text>
-            </View>
-            <View>
-              <Text className="text-xs text-gray-600">{dateRange}</Text>
-            </View>
-          </View>
         </View>
-      </ToProfileButton>
-      {/* Journey Title + Description */}
-      <View className="mb-4">
-        <View className="">
-          <Text className="text-lg font-semibold text-black">{journey.title}</Text>
-        </View>
-        {journey.description && (
-          <View>
-            <Text className="text-sm text-gray-600">{journey.description}</Text>
-          </View>
-        )}
-      </View>
 
-      {/* Stats */}
-      <View className="flex-row justify-between">
-        <View className="flex flex-row items-center gap-1">
-          <MapPin size={16} color="black" fill={'white'} />
-          <Text className="text-sm font-semibold text-gray-600">
-            {journey.locations.length} stop{journey.locations.length === 1 ? '' : 's'}
-          </Text>
-        </View>
-        <View className="flex flex-row items-center gap-1">
-          <Star size={16} color="black" fill="yellow" />
-          <Text className="text-sm font-semibold text-gray-600">{averageRating}</Text>
+        {/* Stats */}
+        <View className="flex-row justify-between">
+          <View className="flex flex-row items-center gap-1">
+            <MapPin size={16} color="black" fill={'white'} />
+            <Text className="text-sm font-semibold text-gray-600">
+              {journey.locations.length} stop{journey.locations.length === 1 ? '' : 's'}
+            </Text>
+          </View>
+          <View className="flex flex-row items-center gap-1">
+            <Star size={16} color="black" fill="yellow" />
+            <Text className="text-sm font-semibold text-gray-600">{averageRating}</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </ToJourneyButton>
   );
 }
 
