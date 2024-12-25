d6927b52531108fe976a1c94fc2ca02cd71552f1
snavemail <liam.jk.evans@gmail.com>
2024-12-25 09:58:45 -0500

    Add ToJourneyButton component for navigation to journey details

---
+++ components/Buttons/ToJourneyButton.tsx
@@ -0,0 +1,23 @@
+import { Pressable } from 'react-native';
+import React from 'react';
+import { useRouter, useSegments } from 'expo-router';
+
+export default function ToJourneyButton({
+  children,
+  journeyID,
+}: {
+  children: React.ReactNode;
+  journeyID: string;
+}) {
+  const segments = useSegments();
+  const router = useRouter();
+
+  return (
+    <Pressable
+      onPress={() => {
+        segments[1] === 'journeys' && router.push(`/(tabs)/${segments[1]}/journey/${journeyID}`);
+      }}>
+      {children}
+    </Pressable>
+  );
+}
