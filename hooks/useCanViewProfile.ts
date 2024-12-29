612aee3e015af70e4b568b70a596d3eb427a9a77
snavemail <liam.jk.evans@gmail.com>
2024-12-28 23:00:48 -0500

    Made hook to block private profiles

---
+++ hooks/useCanViewProfile.ts
@@ -0,0 +1,41 @@
+import { useQuery } from '@tanstack/react-query';
+import { supabase } from '~/lib/supabase';
+import { useAuthStore } from '~/stores/useAuth';
+
+export function useCanViewProfile(profileID: string) {
+  const self = useAuthStore((state) => state.user);
+  const { data } = useQuery({
+    queryKey: ['canViewProfile', profileID],
+    queryFn: async () => {
+      const { data: profile } = await supabase
+        .from('profiles')
+        .select('is_public')
+        .eq('id', profileID)
+        .single();
+
+      if (profileID === self?.id) {
+        console.log('self');
+        return true;
+      }
+
+      if (profile?.is_public) {
+        console.log('public');
+        return true;
+      }
+
+      const { data: isFollowing } = await supabase
+        .from('followers')
+        .select('id')
+        .eq('follower_id', self?.id)
+        .eq('following_id', profileID)
+        .single();
+
+      console.log('isFollowing', isFollowing, !!isFollowing);
+
+      return !!isFollowing;
+    },
+    enabled: !!profileID && !!self?.id,
+  });
+
+  return data ?? false;
+}
