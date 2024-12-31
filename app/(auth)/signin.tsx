57a2972e540f45c2b106cf34aa411456124a20cf
snavemail <liam.jk.evans@gmail.com>
2024-12-31 00:17:21 -0500

    Dark mode and add apple sign in

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,5 +1,14 @@
 // app/(auth)/signin.tsx
-import { View, Text, Pressable, ActivityIndicator, Platform } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  View,
+  Text,
+  Pressable,
+  ActivityIndicator,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { Link, useRouter } from 'expo-router';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -8,6 +17,7 @@ import { FontAwesome } from '@expo/vector-icons';
 import { signInSchema, SignInValues } from '~/lib/validations/auth';
 import { Input } from '~/components/Input';
 import GoogleSignin from '~/components/GoogleSignin';
+import AppleSignin from '~/components/AppleSignin';
 
 export default function SignInScreen() {
   const { signIn, loading } = useAuthStore();
@@ -34,83 +44,76 @@ export default function SignInScreen() {
   };
 
   return (
-    <View className="flex-1 justify-center bg-white p-4">
-      <View className="mt-12 py-8">
-        <Text className="text-3xl font-bold text-gray-900">Sign In</Text>
-      </View>
-
-      <View className="flex flex-col gap-4">
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { onChange, value } }) => (
-            <Input
-              placeholder="Email"
-              value={value}
-              onChangeText={onChange}
-              autoCapitalize="none"
-              keyboardType="email-address"
-              error={errors.email?.message}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="password"
-          render={({ field: { onChange, value } }) => (
-            <Input
-              placeholder="Password"
-              value={value}
-              onChangeText={onChange}
-              secureTextEntry
-              error={errors.password?.message}
-            />
-          )}
-        />
-
-        <Link href="/(auth)/forgot-password" asChild>
-          <Pressable>
-            <Text className="text-right text-black">Forgot password?</Text>
-          </Pressable>
-        </Link>
-
-        <Pressable
-          className="rounded-xl border border-black bg-white p-3"
-          onPress={handleSubmit(onSubmit)}
-          disabled={loading}>
-          {loading ? (
-            <ActivityIndicator color="black" />
-          ) : (
-            <Text className="text-center text-lg font-semibold text-black">Sign In</Text>
-          )}
-        </Pressable>
-      </View>
-
-      {/* Social Auth */}
-      <View className="mt-4">
-        <Text className=" mb-4 text-center text-gray-500">Or continue with</Text>
-
-        <View className="flex flex-row gap-2">
-          <GoogleSignin />
-          {Platform.OS === 'ios' && (
-            <Pressable
-              className="flex-1 flex-row items-center justify-center rounded-xl border border-gray-300 p-3"
-              onPress={() => console.log('Apple')}>
-              <FontAwesome name="apple" size={20} color="black" />
-              <Text className="ml-2 font-medium">Apple</Text>
-            </Pressable>
-          )}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1 justify-center bg-background p-4 dark:bg-background-dark">
+        <View className="mt-12 py-8">
+          <Text className="text-3xl font-bold text-text dark:text-text-dark">Sign In</Text>
         </View>
-      </View>
 
-      {/* Sign Up Link */}
-      <View className="mt-8 flex-row justify-center">
-        <Text className="text-gray-600">Don't have an account? </Text>
-        <Pressable onPress={() => router.replace('/(auth)/signup')}>
-          <Text className="font-semibold text-blue-600">Sign Up</Text>
-        </Pressable>
-      </View>
-    </View>
+        <View className="flex flex-col gap-4">
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Email"
+                value={value}
+                onChangeText={onChange}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                error={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Password"
+                value={value}
+                onChangeText={onChange}
+                secureTextEntry
+                error={errors.password?.message}
+              />
+            )}
+          />
+
+          <Pressable
+            className="mt-2 rounded-xl border border-black bg-background p-3 dark:bg-black"
+            onPress={handleSubmit(onSubmit)}
+            disabled={loading}>
+            {loading ? (
+              <ActivityIndicator color="black" />
+            ) : (
+              <Text className="text-center text-lg font-semibold text-text dark:text-text-dark">
+                Sign In
+              </Text>
+            )}
+          </Pressable>
+        </View>
+
+        {/* Social Auth */}
+        <View className="mt-4">
+          <Text className=" mb-4 text-center text-gray dark:text-gray-dark">Or continue with</Text>
+
+          <View className="flex flex-row gap-2">
+            <GoogleSignin />
+            <AppleSignin />
+          </View>
+        </View>
+
+        {/* Sign Up Link */}
+        <View className="mt-8 flex-row justify-center">
+          <Text className="text-gray dark:text-gray-dark">Don't have an account? </Text>
+          <Pressable onPress={() => router.replace('/(auth)/signup')}>
+            <Text className="font-semibold text-primary dark:text-primary-dark">Sign Up</Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,5 +1,14 @@
 // app/(auth)/signup.tsx
-import { View, Text, TextInput, Pressable, ActivityIndicator } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  View,
+  Text,
+  Pressable,
+  ActivityIndicator,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { useRouter } from 'expo-router';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -8,6 +17,8 @@ import { FontAwesome } from '@expo/vector-icons';
 import { signUpSchema, SignUpValues } from '~/lib/validations/auth';
 import { Input } from '~/components/Input';
 import GoogleSignin from '~/components/GoogleSignin';
+import React from 'react';
+import AppleSignin from '~/components/AppleSignin';
 
 export default function SignUpScreen() {
   const { signUp, loading } = useAuthStore();
@@ -37,123 +48,123 @@ export default function SignUpScreen() {
   };
 
   return (
-    <View className="flex-1 justify-center bg-white p-3">
-      <View className="mt-12 py-8">
-        <Text className="text-3xl font-bold text-gray-900">Sign Up</Text>
-      </View>
-
-      <View className="flex flex-col gap-4">
-        <View className="flex w-full flex-row gap-2">
-          <View className="flex-1">
-            <Controller
-              control={control}
-              name="firstName"
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  placeholder="First Name"
-                  value={value}
-                  onChangeText={onChange}
-                  autoCapitalize="words"
-                  keyboardType="default"
-                />
-              )}
-            />
-          </View>
-          <View className="flex-1">
-            <Controller
-              control={control}
-              name="lastName"
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  placeholder="Last Name"
-                  value={value}
-                  onChangeText={onChange}
-                  autoCapitalize="words"
-                  keyboardType="default"
-                />
-              )}
-            />
-          </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1 justify-center bg-background p-3 dark:bg-background-dark">
+        <View className="mt-12 py-8">
+          <Text className="text-3xl font-bold text-text dark:text-text-dark">Sign Up</Text>
         </View>
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { onChange, value } }) => (
-            <Input
-              placeholder="Email"
-              value={value}
-              onChangeText={onChange}
-              autoCapitalize="none"
-              keyboardType="email-address"
-              error={errors.email?.message}
-            />
-          )}
-        />
 
-        <Controller
-          control={control}
-          name="password"
-          render={({ field: { onChange, value } }) => (
-            <Input
-              placeholder="Password"
-              value={value}
-              onChangeText={onChange}
-              secureTextEntry
-              error={errors.password?.message}
-            />
-          )}
-        />
+        <View className="flex flex-col gap-4">
+          <View className="flex w-full flex-row gap-2">
+            <View className="flex-1">
+              <Controller
+                control={control}
+                name="firstName"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="First Name"
+                    value={value}
+                    onChangeText={onChange}
+                    autoCapitalize="words"
+                    keyboardType="default"
+                  />
+                )}
+              />
+            </View>
+            <View className="flex-1">
+              <Controller
+                control={control}
+                name="lastName"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Last Name"
+                    value={value}
+                    onChangeText={onChange}
+                    autoCapitalize="words"
+                    keyboardType="default"
+                  />
+                )}
+              />
+            </View>
+          </View>
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Email"
+                value={value}
+                onChangeText={onChange}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                error={errors.email?.message}
+              />
+            )}
+          />
 
-        <Controller
-          control={control}
-          name="confirmPassword"
-          render={({ field: { onChange, value } }) => (
-            <Input
-              placeholder="Confirm Password"
-              value={value}
-              onChangeText={onChange}
-              secureTextEntry
-              error={errors.password?.message}
-            />
-          )}
-        />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Password"
+                value={value}
+                onChangeText={onChange}
+                secureTextEntry
+                error={errors.password?.message}
+              />
+            )}
+          />
 
-        {/* Sign Up Button */}
-        <Pressable
-          className="rounded-xl bg-black p-3"
-          onPress={handleSubmit(onSubmit)}
-          disabled={loading}>
-          {loading ? (
-            <ActivityIndicator color="white" />
-          ) : (
-            <Text className="text-center text-lg font-semibold text-white">Create Account</Text>
-          )}
-        </Pressable>
-      </View>
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Confirm Password"
+                value={value}
+                onChangeText={onChange}
+                secureTextEntry
+                error={errors.password?.message}
+              />
+            )}
+          />
 
-      {/* Social Auth */}
-      <View className="mt-4">
-        <Text className="mb-4 text-center text-gray-500">Or continue with</Text>
-
-        <View className="flex flex-row gap-2">
-          <GoogleSignin />
-
+          {/* Sign Up Button */}
           <Pressable
-            className="flex-1 flex-row items-center justify-center rounded-xl border border-gray-300 p-3"
-            onPress={() => alert('Apple')}>
-            <FontAwesome name="apple" size={20} color="black" />
-            <Text className="ml-2 font-medium">Apple</Text>
+            className="rounded-xl bg-black p-3"
+            onPress={handleSubmit(onSubmit)}
+            disabled={loading}>
+            {loading ? (
+              <ActivityIndicator color="white" />
+            ) : (
+              <Text className="text-center text-lg font-semibold text-text dark:text-text-dark">
+                Create Account
+              </Text>
+            )}
           </Pressable>
         </View>
-      </View>
 
-      {/* Sign In Link */}
-      <View className="mt-8 flex-row justify-center">
-        <Text className="text-gray-600">Already have an account? </Text>
-        <Pressable onPress={() => router.replace('/(auth)/signin')}>
-          <Text className="font-semibold text-blue-600">Sign In</Text>
-        </Pressable>
-      </View>
-    </View>
+        {/* Social Auth */}
+        <View className="mt-4">
+          <Text className="mb-4 text-center text-gray dark:text-gray-dark">Or continue with</Text>
+
+          <View className="flex flex-row gap-2">
+            <GoogleSignin />
+            <AppleSignin />
+          </View>
+        </View>
+
+        {/* Sign In Link */}
+        <View className="mt-8 flex-row justify-center">
+          <Text className="text-gray dark:text-gray-dark">Already have an account? </Text>
+          <Pressable onPress={() => router.replace('/(auth)/signin')}>
+            <Text className="font-semibold text-primary dark:text-primary-dark">Sign In</Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
